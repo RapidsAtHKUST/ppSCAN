b904dcd76ececc327399492ccd519126de76a780
CHE Yulin <yche@cse.ust.hk>
2017-09-10 20:24:00 +0800

    add work-efficient workload case study

---
+++ python_experiments/case_studies/figures-case-study5-workload-efficient/ReadMe.md
@@ -0,0 +1,96 @@
+## small_snap_dblp
+
+workload | runtime
+--- | ---
+![small_snap_dblp-workload](../../figures/workload-efficient/small_snap_dblp-min_pts:5-workload.png) | ![small_snap_dblp-runtime](../../figures/workload-efficient/small_snap_dblp-min_pts:5-runtime.png)
+
+tag | eps-0.1 | eps-0.2 | eps-0.3 | eps-0.4 | eps-0.5 | eps-0.6 | eps-0.7 | eps-0.8 | eps-0.9
+--- | --- | --- | --- | --- | --- | --- | --- | --- | ---
+prune0 definitely not reachable | 0.000 | 0.012 | 0.069 | 0.162 | 0.266 | 0.390 | 0.509 | 0.648 | 0.784
+prune1 definitely reachable | 0.717 | 0.396 | 0.194 | 0.103 | 0.051 | 0.017 | 0.004 | 0.002 | 0.000
+max eval number | 0.283 | 0.592 | 0.737 | 0.735 | 0.683 | 0.593 | 0.487 | 0.350 | 0.216
+pscan eval number | 0.037 | 0.369 | 0.618 | 0.610 | 0.496 | 0.352 | 0.230 | 0.123 | 0.051
+pscan+ eval number | 0.030 | 0.318 | 0.569 | 0.627 | 0.598 | 0.510 | 0.401 | 0.266 | 0.156
+serial pscan runtime | 0.285 | 0.430 | 0.504 | 0.486 | 0.426 | 0.316 | 0.236 | 0.165 | 0.118
+serial pscan+ runtime | 0.282 | 0.415 | 0.491 | 0.480 | 0.403 | 0.359 | 0.287 | 0.215 | 0.163
+
+## snap_pokec
+
+workload | runtime
+--- | ---
+![snap_pokec-workload](../../figures/workload-efficient/snap_pokec-min_pts:5-workload.png) | ![snap_pokec-runtime](../../figures/workload-efficient/snap_pokec-min_pts:5-runtime.png)
+
+tag | eps-0.1 | eps-0.2 | eps-0.3 | eps-0.4 | eps-0.5 | eps-0.6 | eps-0.7 | eps-0.8 | eps-0.9
+--- | --- | --- | --- | --- | --- | --- | --- | --- | ---
+prune0 definitely not reachable | 0.004 | 0.021 | 0.061 | 0.131 | 0.233 | 0.370 | 0.525 | 0.689 | 0.850
+prune1 definitely reachable | 0.172 | 0.048 | 0.018 | 0.008 | 0.004 | 0.002 | 0.001 | 0.001 | 0.000
+max eval number | 0.824 | 0.931 | 0.921 | 0.861 | 0.763 | 0.628 | 0.473 | 0.310 | 0.150
+pscan eval number | 0.380 | 0.855 | 0.852 | 0.778 | 0.678 | 0.544 | 0.391 | 0.232 | 0.085
+pscan+ eval number | 0.356 | 0.856 | 0.904 | 0.843 | 0.743 | 0.608 | 0.455 | 0.295 | 0.139
+serial pscan runtime | 7.277 | 10.556 | 9.155 | 7.484 | 5.891 | 4.456 | 3.091 | 1.970 | 1.157
+serial pscan+ runtime | 4.842 | 7.984 | 7.310 | 5.970 | 4.858 | 3.621 | 2.675 | 2.140 | 1.400
+
+## snap_livejournal
+
+workload | runtime
+--- | ---
+![snap_livejournal-workload](../../figures/workload-efficient/snap_livejournal-min_pts:5-workload.png) | ![snap_livejournal-runtime](../../figures/workload-efficient/snap_livejournal-min_pts:5-runtime.png)
+
+tag | eps-0.1 | eps-0.2 | eps-0.3 | eps-0.4 | eps-0.5 | eps-0.6 | eps-0.7 | eps-0.8 | eps-0.9
+--- | --- | --- | --- | --- | --- | --- | --- | --- | ---
+prune0 definitely not reachable | 0.006 | 0.036 | 0.097 | 0.186 | 0.295 | 0.426 | 0.564 | 0.706 | 0.849
+prune1 definitely reachable | 0.206 | 0.069 | 0.029 | 0.014 | 0.007 | 0.003 | 0.002 | 0.001 | 0.000
+max eval number | 0.787 | 0.895 | 0.874 | 0.800 | 0.698 | 0.571 | 0.435 | 0.293 | 0.151
+pscan eval number | 0.261 | 0.643 | 0.724 | 0.676 | 0.576 | 0.450 | 0.320 | 0.192 | 0.076
+pscan+ eval number | 0.218 | 0.588 | 0.714 | 0.712 | 0.634 | 0.517 | 0.389 | 0.257 | 0.129
+serial pscan runtime | 18.827 | 26.597 | 25.283 | 19.445 | 15.217 | 11.016 | 7.507 | 4.590 | 2.375
+serial pscan+ runtime | 9.805 | 17.607 | 17.497 | 14.925 | 11.975 | 8.635 | 6.238 | 4.283 | 2.811
+
+## snap_orkut
+
+workload | runtime
+--- | ---
+![snap_orkut-workload](../../figures/workload-efficient/snap_orkut-min_pts:5-workload.png) | ![snap_orkut-runtime](../../figures/workload-efficient/snap_orkut-min_pts:5-runtime.png)
+
+tag | eps-0.1 | eps-0.2 | eps-0.3 | eps-0.4 | eps-0.5 | eps-0.6 | eps-0.7 | eps-0.8 | eps-0.9
+--- | --- | --- | --- | --- | --- | --- | --- | --- | ---
+prune0 definitely not reachable | 0.010 | 0.034 | 0.072 | 0.135 | 0.227 | 0.350 | 0.497 | 0.661 | 0.831
+prune1 definitely reachable | 0.009 | 0.001 | 0.000 | 0.000 | 0.000 | 0.000 | 0.000 | 0.000 | 0.000
+max eval number | 0.980 | 0.965 | 0.928 | 0.865 | 0.773 | 0.650 | 0.503 | 0.339 | 0.169
+pscan eval number | 0.495 | 0.727 | 0.826 | 0.822 | 0.746 | 0.626 | 0.478 | 0.313 | 0.142
+pscan+ eval number | 0.444 | 0.688 | 0.817 | 0.836 | 0.766 | 0.648 | 0.502 | 0.338 | 0.168
+serial pscan runtime | 176.912 | 201.380 | 195.265 | 152.046 | 104.250 | 87.674 | 52.443 | 29.814 | 12.790
+serial pscan+ runtime | 95.373 | 138.914 | 133.177 | 112.870 | 76.988 | 57.462 | 39.855 | 22.289 | 10.276
+
+## webgraph_uk
+
+workload | runtime
+--- | ---
+![webgraph_uk-workload](../../figures/workload-efficient/webgraph_uk-min_pts:5-workload.png) | ![webgraph_uk-runtime](../../figures/workload-efficient/webgraph_uk-min_pts:5-runtime.png)
+
+tag | eps-0.1 | eps-0.2 | eps-0.3 | eps-0.4 | eps-0.5 | eps-0.6 | eps-0.7 | eps-0.8 | eps-0.9
+--- | --- | --- | --- | --- | --- | --- | --- | --- | ---
+prune0 definitely not reachable | 0.064 | 0.272 | 0.449 | 0.558 | 0.631 | 0.690 | 0.738 | 0.785 | 0.833
+prune1 definitely reachable | 0.144 | 0.064 | 0.033 | 0.020 | 0.012 | 0.005 | 0.003 | 0.002 | 0.000
+max eval number | 0.792 | 0.665 | 0.517 | 0.422 | 0.358 | 0.305 | 0.259 | 0.213 | 0.166
+pscan eval number | 0.217 | 0.261 | 0.244 | 0.212 | 0.177 | 0.145 | 0.118 | 0.090 | 0.063
+pscan+ eval number | 0.235 | 0.269 | 0.250 | 0.219 | 0.188 | 0.159 | 0.132 | 0.103 | 0.074
+serial pscan runtime | 36.924 | 25.810 | 18.501 | 14.319 | 11.455 | 9.821 | 8.127 | 6.951 | 5.683
+serial pscan+ runtime | 21.600 | 16.999 | 13.872 | 11.188 | 9.854 | 9.372 | 8.629 | 7.782 | 6.829
+
+## webgraph_webbase
+
+workload | runtime
+--- | ---
+![webgraph_webbase-workload](../../figures/workload-efficient/webgraph_webbase-min_pts:5-workload.png) | ![webgraph_webbase-runtime](../../figures/workload-efficient/webgraph_webbase-min_pts:5-runtime.png)
+
+tag | eps-0.1 | eps-0.2 | eps-0.3 | eps-0.4 | eps-0.5 | eps-0.6 | eps-0.7 | eps-0.8 | eps-0.9
+--- | --- | --- | --- | --- | --- | --- | --- | --- | ---
+prune0 definitely not reachable | 0.080 | 0.265 | 0.388 | 0.482 | 0.555 | 0.625 | 0.684 | 0.751 | 0.813
+prune1 definitely reachable | 0.294 | 0.155 | 0.088 | 0.054 | 0.033 | 0.014 | 0.008 | 0.005 | 0.001
+max eval number | 0.626 | 0.581 | 0.524 | 0.463 | 0.412 | 0.361 | 0.308 | 0.244 | 0.186
+pscan eval number | 0.220 | 0.281 | 0.287 | 0.256 | 0.215 | 0.170 | 0.135 | 0.097 | 0.065
+pscan+ eval number | 0.244 | 0.296 | 0.301 | 0.273 | 0.241 | 0.202 | 0.166 | 0.123 | 0.085
+serial pscan runtime | 146.030 | 89.716 | 75.893 | 62.764 | 52.310 | 60.173 | 34.236 | 27.175 | 21.852
+serial pscan+ runtime | 93.338 | 60.988 | 48.694 | 45.878 | 40.025 | 37.471 | 34.481 | 31.221 | 25.799
+
--- python_experiments/workload_figure.py
+++ python_experiments/workload_figure.py
@@ -1,4 +1,5 @@
 import os, re
+import decimal
 from functools import partial
 
 import matplotlib.pyplot as plt
@@ -18,6 +19,10 @@ runtime_tag = 'Total time without IO'
 # suffix
 pscan_tag = ', pscan'
 pscan_plus_tag = ', pscan+'
+
+
+def format_str(float_num):
+    return str(decimal.Decimal.from_float(float_num).quantize(decimal.Decimal('0.000')))
 
 
 def filter_lines_by_tag(tag, lines):
@@ -90,18 +95,22 @@ def to_display_dict(statistics_dict, min_runtime_val):
     return display_dict
 
 
-def display_workload_runtime(eps_lst, display_lst, title_append_txt=''):
+def display_workload_runtime(eps_lst, display_lst, title_append_txt='', is_display_naive=False):
     tag_list = [display_pscan_plus_eval_tag, display_pscan_eval_tag,
                 display_prune0_tag, display_prune1_tag]
 
     # draw after get data, with partial binding technique
     shape_color_lst = ['g^-', 'rs-', 'c<-', 'y>-', 'mx-', 'k--']
-    prev_partial_func = plt.plot
-    cur_shape_color_idx = 0
 
     result_lst = map(lambda tag:
                      map(lambda display: display[tag], display_lst), tag_list)
+    if is_display_naive:
+        shape_color_lst = ['b.-', 'g^-', 'rs-', 'c<-', 'y>-', 'mx-', 'k--']
+        result_lst = [map(lambda my_pair: 1 - my_pair[0] - my_pair[1],
+                          zip(result_lst[-1], result_lst[-2]))] + result_lst
 
+    prev_partial_func = plt.plot
+    cur_shape_color_idx = 0
     for portion_lst in result_lst:
         # partially bind parameters
         prev_partial_func = partial(prev_partial_func, eps_lst, portion_lst,
@@ -109,18 +118,26 @@ def display_workload_runtime(eps_lst, display_lst, title_append_txt=''):
         cur_shape_color_idx += 1
     prev_partial_func()
 
-    plt.legend(tag_list)
+    plt.legend(['max eval number'] + tag_list)
     font = {'family': 'serif', 'color': 'darkred', 'weight': 'normal', 'size': 12, }
     plt.title('Workload portion\n' + title_append_txt
               if title_append_txt != '' else 'Workload portion', fontdict=font)
     plt.xlabel('eps', fontdict=font)
     plt.ylabel('portion', fontdict=font)
-    plt.ylim([-0.1, 1.1])
-    os.system('mkdir -p ./figures/workload')
-    plt.savefig('./figures/workload' + os.sep + title_append_txt.replace(' ', '') + '-' + 'workload.png',
+    plt.ylim([0.0, 1.1])
+    plt.savefig(local_folder + os.sep + title_append_txt.replace(' ', '') + '-' + 'workload.png',
                 bbox_inches='tight', pad_inches=0, transparent=True)
-    # plt.show()
+
+    table_rows = [' | '.join(['tag'] + map(lambda eps_val: 'eps-' + str(eps_val), eps_lst)),
+                  ' | '.join(['---' for _ in xrange(len(eps_lst) + 1)]),
+                  ' | '.join([display_prune0_tag] + map(format_str, result_lst[-2])),
+                  ' | '.join([display_prune1_tag] + map(format_str, result_lst[-1])),
+                  ' | '.join(['max eval number'] + map(format_str, result_lst[0])),
+                  ' | '.join([display_pscan_eval_tag] + map(format_str, result_lst[2])),
+                  ' | '.join([display_pscan_plus_eval_tag] + map(format_str, result_lst[1]))]
+
     plt.close()
+    return '\n'.join(table_rows)
 
 
 def display_runtime(eps_lst, display_lst, title_append_txt=''):
@@ -148,23 +165,23 @@ def display_runtime(eps_lst, display_lst, title_append_txt=''):
     plt.xlabel('eps', fontdict=font)
     plt.ylabel('time (s)', fontdict=font)
 
-    os.system('mkdir -p ./figures/workload')
-    plt.savefig('./figures/workload' + os.sep + title_append_txt.replace(' ', '') + '-' + 'runtime.png',
+    max_val = max(max(result_lst[0]), max(result_lst[1]))
+    plt.ylim([0.0, max_val * 1.1])
+    plt.savefig(local_folder + os.sep + title_append_txt.replace(' ', '') + '-' + 'runtime.png',
                 bbox_inches='tight', pad_inches=0, transparent=True)
     # plt.show()
     plt.close()
 
+    table_rows = [' | '.join([display_pscan_runtime_tag] + map(format_str, result_lst[1])),
+                  ' | '.join([display_pscan_plus_runtime_tag] + map(format_str, result_lst[0])), ]
+    return '\n'.join(table_rows)
+
 
 def case_study0():
     # case study 0
-    data_set_lst = ['small_snap_dblp',
-                    'snap_pokec', 'snap_livejournal', 'snap_orkut',
-                    'webgraph_uk', 'webgraph_webbase',
-                    'webgraph_twitter', 'snap_friendster',
-                    '10million_avgdeg15_maxdeg50_Cdefault']
     eps = 0.3
     min_pts = 5
-    root_dir_path = '/mnt/mount-gpu/d2/yche/projects/python_experiments/worklaod'
+    root_dir_path = server_folder
     case_study_fix_eps_min_pts(data_set_lst, eps, min_pts, root_dir_path)
     print
 
@@ -172,23 +189,47 @@ def case_study0():
 def case_study1():
     # case study 1
     min_pts = 5
-    root_dir_path = '/mnt/mount-gpu/d2/yche/projects/python_experiments/worklaod'
+    root_dir_path = server_folder
+
+    with open(markdown_folder + os.sep + 'ReadMe.md', 'w') as ofs:
+        parameter_eps_lst = [float(i + 1) / 10 for i in xrange(9)]
+        for data_set in data_set_lst:
+            statistics_lst = map(lambda eps: get_workload_statistics(data_set, eps, min_pts, root_dir_path),
+                                 parameter_eps_lst)
+            display_lst = map(lambda statistics_dict: to_display_dict(statistics_dict, 1000), statistics_lst)
+            append_txt = ' - '.join([data_set, 'min_pts:' + str(min_pts)])
+            table_lines = display_workload_runtime(parameter_eps_lst, display_lst, title_append_txt=append_txt,
+                                                   is_display_naive=True)
+            runtime_rows = display_runtime(parameter_eps_lst, display_lst, title_append_txt=append_txt)
+
+            lines = ['## ' + data_set]
+
+            def get_link():
+                workload_link = local_folder.replace('.', '../..') + os.sep + append_txt.replace(' ',
+                                                                                                 '') + '-' + 'workload.png'
+                runtime_link = local_folder.replace('.', '../..') + os.sep + append_txt.replace(' ',
+                                                                                                '') + '-' + 'runtime.png'
+                return ' | '.join(['![' + data_set + '-workload](' + workload_link + ')',
+                                   '![' + data_set + '-runtime](' + runtime_link + ')'])
+
+            lines.append('\n'.join(['workload | runtime\n--- | ---', get_link()]))
+            lines.append('\n'.join([table_lines, runtime_rows]))
+            ofs.write('\n\n'.join(lines) + '\n\n')
+
+
+if __name__ == '__main__':
+    server_folder = '/mnt/mount-gpu/d2/yche/projects/python_experiments/workload-work-efficient'
+    local_folder = './figures/' + 'workload-efficient'
+    markdown_folder = 'case_studies/figures-case-study5-workload-efficient'
+    os.system('mkdir -p ' + local_folder)
+    os.system('mkdir -p ' + markdown_folder)
 
     data_set_lst = ['small_snap_dblp',
                     'snap_pokec', 'snap_livejournal', 'snap_orkut',
                     'webgraph_uk', 'webgraph_webbase',
-                    'webgraph_twitter', 'snap_friendster',
-                    '10million_avgdeg15_maxdeg50_Cdefault']
-    parameter_eps_lst = [float(i + 1) / 10 for i in xrange(9)]
-    for data_set in data_set_lst:
-        statistics_lst = map(lambda eps: get_workload_statistics(data_set, eps, min_pts, root_dir_path),
-                             parameter_eps_lst)
-        display_lst = map(lambda statistics_dict: to_display_dict(statistics_dict, 1000), statistics_lst)
-        append_txt = ' - '.join([data_set, 'min_pts:' + str(min_pts)])
-        display_workload_runtime(parameter_eps_lst, display_lst, title_append_txt=append_txt)
-        display_runtime(parameter_eps_lst, display_lst, title_append_txt=append_txt)
+                    # 'webgraph_twitter', 'snap_friendster',
+                    # '10million_avgdeg15_maxdeg50_Cdefault'
+                    ]
 
-
-if __name__ == '__main__':
     case_study0()
     case_study1()
