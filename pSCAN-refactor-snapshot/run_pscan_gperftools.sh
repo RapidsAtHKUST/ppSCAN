11663fda97812ad56c0340720044c62de0c9c9ed
CHE Yulin <yche@cse.ust.hk>
2017-09-26 13:59:46 +0800

    remove useless

--- pSCAN-refactor-snapshot/run_pscan_gperftools.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-build/pSCANStatistics0 $1 0.3 5 output
-
-# convert profile.log to callgrind compatible format
-pprof --callgrind build/pSCANStatistics0 pscanProfile.log > profile.callgrind
