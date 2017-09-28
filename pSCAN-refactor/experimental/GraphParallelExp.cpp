2ee265f3add9d9b90003c00614e74a34a24f5bec
CHE Yulin <yche@cse.ust.hk>
2017-09-28 16:56:40 +0800

    adopt future instead of mutex to become lock-free

--- pSCAN-refactor/experimental/GraphParallelExp.cpp
+++ pSCAN-refactor/experimental/GraphParallelExp.cpp
@@ -463,31 +463,33 @@ void GraphParallelExp::pSCANFourthPhaseClusterNonCore() {
         ThreadPool pool(thread_num_);
 
         auto batch_size = 8192u;
-        mutex non_core_cluster_mutex;
+        vector<future<vector<pair<int, int >>>> future_vec;
         for (auto v_i = 0; v_i < cores.size(); v_i += batch_size) {
             int my_start = v_i;
             int my_end = min(static_cast<ui>(cores.size()), my_start + batch_size);
-            pool.enqueue([this, &non_core_cluster_mutex](int i_start, int i_end) {
-                auto tmp_cluster = vector<pair<int, int>>();
-                for (auto i = i_start; i < i_end; i++) {
-                    auto u = cores[i];
-                    for (auto j = out_edge_start[u]; j < out_edge_start[u + 1]; j++) {
-                        auto v = out_edges[j];
-                        if (!IsDefiniteCoreVertex(v)) {
-                            auto root_of_u = disjoint_set_ptr->FindRoot(static_cast<uint32_t>(u));
-                            if (min_cn[j] == SIMILAR) {
-                                tmp_cluster.emplace_back(cluster_dict[root_of_u], v);
+            future_vec.emplace_back(
+                    pool.enqueue([this](int i_start, int i_end) -> vector<pair<int, int>> {
+                        auto tmp_cluster = vector<pair<int, int>>();
+                        for (auto i = i_start; i < i_end; i++) {
+                            auto u = cores[i];
+                            for (auto j = out_edge_start[u]; j < out_edge_start[u + 1]; j++) {
+                                auto v = out_edges[j];
+                                if (!IsDefiniteCoreVertex(v)) {
+                                    auto root_of_u = disjoint_set_ptr->FindRoot(static_cast<uint32_t>(u));
+                                    if (min_cn[j] == SIMILAR) {
+                                        tmp_cluster.emplace_back(cluster_dict[root_of_u], v);
+                                    }
+                                }
                             }
                         }
-                    }
-                }
-                {
-                    unique_lock<mutex> lock(non_core_cluster_mutex);
-                    for (auto ele: tmp_cluster) {
-                        noncore_cluster.emplace_back(ele);
-                    }
-                }
-            }, my_start, my_end);
+                        return tmp_cluster;
+                    }, my_start, my_end));
+        }
+
+        for (auto &future: future_vec) {
+            for (auto ele:future.get()) {
+                noncore_cluster.emplace_back(ele);
+            };
         }
     }
     auto all_end = high_resolution_clock::now();
