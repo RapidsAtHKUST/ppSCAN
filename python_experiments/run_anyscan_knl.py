efea18863c57a058a3d62353004e56be27d4a287
CHE Yulin <yche@cse.ust.hk>
2017-11-17 11:18:06 +0800

    update exp script

---
+++ python_experiments/run_anyscan_knl.py
@@ -0,0 +1,54 @@
+import time_out_util
+import os, time
+
+if __name__ == '__main__':
+    any_scan_path = '/nfsshare/share/anySCANBin/anyscan1114'
+    data_set_lst = ['snap_livejournal',
+                    # 'snap_orkut',
+                    # 'webgraph_webbase', 'webgraph_twitter',
+                    # 'snap_friendster'
+                    ]
+    parameter_eps_lst = [float(i + 1) / 10 for i in xrange(9)]
+    parameter_min_pts_lst = [5]
+
+    data_set_lst = map(lambda name: os.pardir + os.sep + 'dataset' + os.sep + name, data_set_lst)
+    thread_num = 256
+    foler_name = 'anyscan-exp'
+    for data_set_path in data_set_lst:
+        for eps in parameter_eps_lst:
+            for min_pts in parameter_min_pts_lst:
+                # statistics output dir and file
+                data_set_name = data_set_path.split(os.sep)[-1]
+                statistics_dir = os.sep.join(
+                    map(str, ['.', foler_name, data_set_name, 'eps-' + str(eps), 'min_pts-' + str(min_pts)]))
+                os.system('mkdir -p ' + statistics_dir)
+                statistics_file_path = statistics_dir + os.sep + '-'.join(
+                    map(str, ['output', data_set_name, eps, min_pts])) + '.txt'
+
+                # 1st: splitter, record start time
+                my_splitter = '-'.join(['*' for _ in xrange(20)])
+                os.system(' '.join(
+                    ['echo', my_splitter + time.ctime() + my_splitter, '>>', statistics_file_path]))
+
+
+                def write_split():
+                    with open(statistics_file_path, 'a+') as ifs:
+                        ifs.write(my_splitter + my_splitter + '\n')
+                        ifs.write(my_splitter + my_splitter + '\n')
+
+
+                params_lst = map(str, [any_scan_path,
+                                       '-c', 4, '-g', 'label.gold',
+                                       '-i', data_set_path, '-e', eps, '-m', min_pts,
+                                       '-a', 32768, '-b', 32768, '-t', thread_num, '>>', statistics_file_path])
+                cmd = ' '.join(params_lst)
+                print cmd
+                tle_flag, info = time_out_util.run_with_timeout(cmd, timeout_sec=3600)
+                write_split()
+
+                with open(statistics_file_path, 'a+') as ifs:
+                    ifs.write(my_splitter + time.ctime() + my_splitter)
+                    ifs.write('is_time_out:' + str(tle_flag))
+                    ifs.write('\n\n\n\n')
+                print 'finish:', '-'.join(map(str, [data_set_path, eps, min_pts]))
+    print
---
+++ python_experiments/time_out_util.py
@@ -0,0 +1,31 @@
+import os, sys, commands
+import subprocess, shlex, time
+from threading import Timer
+
+
+def run_with_timeout(cmd, timeout_sec):
+    global is_time_out
+    is_time_out = False
+    proc = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
+
+    def time_out_call_back(p):
+        print 'time out need to kill'
+        global is_time_out
+        is_time_out = True
+        p.kill()
+
+    timer = Timer(timeout_sec, time_out_call_back, [proc])
+    try:
+        timer.start()
+        stdout, stderr = proc.communicate()
+        print str(stderr).replace('\n', '')
+    finally:
+        timer.cancel()
+    # print is_time_out
+    return is_time_out, str(stderr).replace('\n', '')
+
+
+if __name__ == '__main__':
+    tle_flag, info = run_with_timeout('sleep 20', timeout_sec=5)
+    print tle_flag, info
