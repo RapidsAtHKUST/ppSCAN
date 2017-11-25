74fda5ab329586161a65fa0bc48ed272719a8093
CHE Yulin <yche@cse.ust.hk>
2017-11-26 00:07:35 +0800

    update exp for paper, some data to be confirmed

--- paper/scalability_roll_graph/generate_self_markdown.py
+++ paper/scalability_roll_graph/generate_self_markdown.py
@@ -29,26 +29,34 @@ def get_statistics(dataset, eps, min_pts, graph_folder, root_folder='.'):
     file_name = '-'.join(map(str, ['output', dataset, eps, min_pts]))
     file_path = dir_path + os.sep + file_name + '.txt'
     with open(file_path) as ifs:
-        time_pair = parse_lines(ifs.readlines())
+        tmp_lines = parse_lines(ifs.readlines())
+        time_pair = (min(tmp_lines), max(tmp_lines))
         time_pair = map(lambda time: float(time) / 1000, time_pair)
         time_pair.append(time_pair[1] / time_pair[0])
         time_pair.insert(0, str(eps))
         return time_pair
 
 
-if __name__ == '__main__':
+def generate_per_env(root_folder, data_folder, output_folder):
     min_pts = 5
     for dataset in ['n50_m20_1billion_edge', 'n25_m40_1billion_edge', 'n16dot7_m60_1billion_edge',
-                    # 'n12dot5_m80_1billion_edge'
+                    'n12dot5_m80_1billion_edge'
                     ]:
         print '\n', 'dataset:', dataset
         row_lst = []
         for eps in [float(i + 1) / 10 for i in xrange(9)]:
-            single_eps_data = get_statistics(dataset, eps, min_pts, 'synthetic-graph-roll-self',
-                                             root_folder='/home/yche/mnt/gpu-23/projects/python_experiments')
+            single_eps_data = get_statistics(dataset, eps, min_pts, data_folder, root_folder=root_folder)
             row_lst.append(single_eps_data)
-        os.system('mkdir -p self-speedup')
-        with open('self-speedup' + os.sep + dataset + '.md', 'w') as ofs:
-            ofs.write('eps | ppscan time | pscan time | speedup over pscan\n')
+        if not os.path.exists(output_folder):
+            os.mkdir(output_folder)
+        with open(output_folder + os.sep + dataset + '.md', 'w') as ofs:
+            ofs.write('eps | ppscan time | ppscan serial time | self-speedup \n')
             ofs.write('--- | --- | --- | ---\n')
             ofs.write('\n'.join(map(lambda row: ' | '.join(map(format_str, row)), row_lst)) + '\n')
+
+
+if __name__ == '__main__':
+    generate_per_env('/home/yche/mnt/luocpu8/nfsshare/share/python_experiments', 'paper-roll-graph-exp-11-24-knl',
+                     'self-speedup-knl')
+    generate_per_env('/home/yche/mnt/gpu-23/projects/python_experiments', 'paper-roll-graph-exp-11-24-gpu23',
+                     'self-speedup-gpu23')
--- paper/scalability_roll_graph/scalability_roll_paper.py
+++ paper/scalability_roll_graph/scalability_roll_paper.py
@@ -3,8 +3,8 @@ import os
 
 
 def process_dataset(dataset):
-    with open(dataset + '.md') as ifs:
-        lines = ifs.readlines()[2:10]
+    with open('self-speedup-gpu23' + os.sep + dataset + '.md') as ifs:
+        lines = ifs.readlines()[2:11]
         data_lst = map(lambda line: line.strip().split(' | '), lines)
         ppscan_runtime_lst = map(lambda data: float(data[1]), data_lst)
         speedup_lst = map(lambda data: float(data[3]), data_lst)
@@ -12,6 +12,9 @@ def process_dataset(dataset):
 
 
 if __name__ == '__main__':
+    if not os.path.exists('figures'):
+        os.mkdir('figures')
+
     ppscan_runtime_lst_lst = []
     ppscan_speedup_lst_lst = []
     for dataset in ['n50_m20_1billion_edge', 'n25_m40_1billion_edge', 'n16dot7_m60_1billion_edge',
@@ -23,7 +26,7 @@ if __name__ == '__main__':
 
     print ppscan_runtime_lst_lst
     print ppscan_speedup_lst_lst
-    eps_lst = [float(i + 1) / 10 for i in range(8)]
+    eps_lst = [float(i + 1) / 10 for i in range(9)]
 
     exp2_figure, ax_tuple = plt.subplots(1, 2, sharex=True, figsize=(8, 3))
     legend_lst = ['avg deg = 40', 'avg deg = 80', 'avg deg = 120', 'avg deg = 160']
@@ -33,9 +36,9 @@ if __name__ == '__main__':
     for idx, runtime_lst in enumerate(ppscan_runtime_lst_lst):
         ax.plot(eps_lst, runtime_lst, shape_lst[idx], markersize=10, markerfacecolor='none')
     ax.legend(legend_lst)
-    ax.set_ylim(0, max(map(max, ppscan_runtime_lst_lst)) * 1.1)
+    # ax.set_ylim(0, max(map(max, ppscan_runtime_lst_lst)) * 1.1)
     ax.set_ylabel('Runtime (s)', fontsize=12)
-    ax.set_title('undirected edge number = 1 billion', fontsize=12)
+    ax.set_title('on CPU', fontsize=12)
     ax.set_xlabel('$\\epsilon = $')
     ax.xaxis.set_label_coords(0.00, -0.045)
     ax.grid(True)
@@ -45,14 +48,14 @@ if __name__ == '__main__':
     for idx, speedup_lst in enumerate(ppscan_speedup_lst_lst):
         ax.plot(eps_lst, speedup_lst, shape_lst[idx], markersize=10, markerfacecolor='none')
     ax.legend(legend_lst)
-    ax.set_ylim(0, max(map(max, ppscan_speedup_lst_lst)) * 1.1)
-    ax.set_ylabel('Speedup over pSCAN', fontsize=12)
-    ax.set_title('undirected edge number = 1 billion', fontsize=12)
+    # ax.set_ylim(0, max(map(max, ppscan_speedup_lst_lst)) * 1.1)
+    ax.set_ylabel('Self-Speedup', fontsize=12)
+    ax.set_title('on CPU', fontsize=12)
     ax.set_xlabel('$\\epsilon = $')
     ax.xaxis.set_label_coords(0.00, -0.045)
     ax.grid(True)
 
     exp2_figure.subplots_adjust(wspace=0)
     plt.tight_layout()
-    plt.savefig('scalability_roll_graph.' + 'pdf', dpi=1200)
+    plt.savefig('figures/scalability_roll_graph_gpu23.' + 'pdf', dpi=1200)
     exp2_figure.show()
